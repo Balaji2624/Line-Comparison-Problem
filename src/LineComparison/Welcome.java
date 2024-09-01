5f3f64e70b38e90fc72ee3275611f7302b3923f7
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-01 10:26:22 +0530

    welcome to line comparison problem

---
+++ src/LineComparison/Welcome.java
@@ -0,0 +1,9 @@
+package LineComparison;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line\n" +
+                "Comparison Computation\n" +
+                "Program");
+    }
+}
